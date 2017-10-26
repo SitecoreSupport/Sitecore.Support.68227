070f308b76687108bce3c31090b5d3a91c22c5e1
DK\YMU <ymu@sitecore.net>
2017-10-26 12:20:43 +0300

    Fix the bug #68227

--- src/Sitecore.Support.68227/EditRenderingProperties.cs
+++ src/Sitecore.Support.68227/EditRenderingProperties.cs
@@ -7,14 +7,19 @@ using Sitecore.Shell.Framework.Commands;
 using Sitecore.Web;
 using Sitecore.Web.UI.Sheer;
 using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Linq;
 
-namespace Sitecore.Shell.Applications.WebEdit.Commands
+namespace Sitecore.Support.Shell.Applications.WebEdit.Commands
 {
   /// <summary>Implements the Edit command.</summary>
   [System.Serializable]
   public class EditRenderingProperties : Command
   {
+    // Fields
+    private static List<OpenedItems> openedItems = new List<OpenedItems>();
+
     // Methods
     public override void Execute(CommandContext context)
     {
@@ -22,17 +27,38 @@ namespace Sitecore.Shell.Applications.WebEdit.Commands
       string formValue = WebUtil.GetFormValue("scLayout");
       string id = ShortID.Decode(WebUtil.GetFormValue("scDeviceID"));
       string uniqueId = ShortID.Decode(context.Parameters["uniqueid"]);
-      string key = "PageDesigner";
-      string str5 = Sitecore.Web.WebEditUtil.ConvertJSONLayoutToXML(formValue);
-      Assert.IsNotNull(str5, "xml");
-      WebUtil.SetSessionValue(key, str5);
-      int index = LayoutDefinition.Parse(str5).GetDevice(id).GetIndex(uniqueId);
+      string str4 = Sitecore.Web.WebEditUtil.ConvertJSONLayoutToXML(formValue);
+      Assert.IsNotNull(str4, "xml");
+      OpenedItems myclass = new OpenedItems();
+      string key = "PageDesigner" + openedItems.Count.ToString();
+      WebUtil.SetSessionValue(key, str4);
+      myclass.Pagedesigner = key;
+      int index = LayoutDefinition.Parse(str4).GetDevice(id).GetIndex(uniqueId);
       NameValueCollection parameters = new NameValueCollection
       {
         ["device"] = id,
-        ["handle"] = key,
         ["selectedindex"] = index.ToString()
       };
+      myclass.ItemId = context.Items.First<Item>().ID.ToString();
+      string str6 = "handle" + openedItems.Count.ToString();
+      myclass.Handle = str6;
+      if (openedItems.Count > 0)
+      {
+        int num3 = openedItems.FindIndex(x => x.ItemId.Equals(myclass.ItemId));
+        if (num3 >= 0)
+        {
+          openedItems[num3] = myclass;
+        }
+        else
+        {
+          openedItems.Add(myclass);
+        }
+      }
+      else
+      {
+        openedItems.Add(myclass);
+      }
+      parameters[str6] = key;
       ClientPipelineArgs args = new ClientPipelineArgs(parameters);
       Context.ClientPage.Start(this, "Run", args);
     }
@@ -43,6 +69,11 @@ namespace Sitecore.Shell.Applications.WebEdit.Commands
       return Sitecore.Web.WebEditUtil.ConvertXMLLayoutToJSON(layout);
     }
 
+    private void RemoveOpenedItemFromSession(OpenedItems singleobject)
+    {
+      openedItems.Remove(singleobject);
+    }
+
     protected void Run(ClientPipelineArgs args)
     {
       Assert.ArgumentNotNull(args, "args");
@@ -50,27 +81,38 @@ namespace Sitecore.Shell.Applications.WebEdit.Commands
       if (@int >= 0)
       {
         Item clientContentItem = Sitecore.Web.WebEditUtil.GetClientContentItem(Client.ContentDatabase);
+        OpenedItems item = openedItems.Single<OpenedItems>(r => r.ItemId.Equals(clientContentItem.ID.ToString()));
         RenderingParameters parameters = new RenderingParameters
         {
           Args = args,
           DeviceId = args.Parameters["device"],
           SelectedIndex = @int,
-          HandleName = args.Parameters["handle"],
+          HandleName = args.Parameters[item.Handle],
           Item = clientContentItem
         };
         if (parameters.Show())
         {
           if (args.HasResult)
           {
-            string layout = GetLayout(WebUtil.GetSessionString(args.Parameters["handle"]));
-            SheerResponse.SetAttribute("scLayoutDefinition", "value", layout);
-            SheerResponse.Eval("window.parent.Sitecore.PageModes.ChromeManager.handleMessage('chrome:rendering:propertiescompleted');");
+            string sessionString = WebUtil.GetSessionString(args.Parameters[item.Handle]);
+            if (!string.IsNullOrEmpty(sessionString))
+            {
+              string layout = GetLayout(sessionString);
+              SheerResponse.SetAttribute("scLayoutDefinition", "value", layout);
+              SheerResponse.Eval("window.parent.Sitecore.PageModes.ChromeManager.handleMessage('chrome:rendering:propertiescompleted');");
+            }
+            else
+            {
+              Log.Warn("We were protected from issue because of simultaneous editing", this);
+              SheerResponse.SetAttribute("scLayoutDefinition", "value", string.Empty);
+            }
           }
           else
           {
             SheerResponse.SetAttribute("scLayoutDefinition", "value", string.Empty);
           }
-          WebUtil.RemoveSessionValue(args.Parameters["handle"]);
+          WebUtil.RemoveSessionValue(args.Parameters[item.Handle]);
+          openedItems.Remove(item);
         }
       }
     }
---
+++ src/Sitecore.Support.68227/OpenedItems.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Sitecore.Support.Shell.Applications.WebEdit.Commands
+{
+  internal class OpenedItems
+  {
+    public string ItemId
+    {
+      get;
+      set;
+    }
+
+    public string Handle
+    {
+      get;
+      set;
+    }
+
+    public string Pagedesigner
+    {
+      get;
+      set;
+    }
+  }
+}
